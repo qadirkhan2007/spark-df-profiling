0070594c1b68579c6f65eee7a0f3e794a75e856f
qadirkhan2007 <49492570+qadirkhan2007@users.noreply.github.com>
2021-03-29 17:41:38 -0400

    Update __init__.py

--- spark_df_profiling/__init__.py
+++ spark_df_profiling/__init__.py
@@ -43,7 +43,10 @@ class ProfileReport(object):
 
         description_set = describe(df, bins=bins, corr_reject=corr_reject, config=config, **kwargs)
         
-        print(pretty(description_set))
+        #print(pretty(description_set))
+        print(pretty(description_set['table']))
+        print(pretty(description_set['variables']))
+        print(pretty(description_set['freq']))
       
         #print("TESTING")
         #pretty(description_set, 0)
